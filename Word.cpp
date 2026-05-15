fb6fc504708cf9fd174802037dd6d4d56f4f4f34
mostafahh1 <mmoossttaaffaa6666663@gmail.com>
2026-05-15 16:25:16 -0700

    solve the problem successfully

--- Word.cpp
+++ Word.cpp
@@ -2,8 +2,36 @@
 using namespace std;
 
 int main(){
+    string word;
+    short n_lower = 0 , n_greater = 0;
+
+    cin >> word;
+
+    for (char Char : word){
+        if ((Char > 64) && (Char < 91))
+            n_greater++;
+        else
+            n_lower++;
+    }
+
+
+    if (n_greater > n_lower){
+        for (char Char : word){
+            if ((Char > 96) && (Char < 123)){
+                Char = Char - 32;
+            }
+            cout << Char;
+        }
+
+    }else{
+        for (char Char : word){
+            if ((Char > 64) && (Char < 91)){
+                Char = Char + 32;
+            }
+            cout << Char;
+        }
+    }
 
     
-    cout << "sad" << endl;
     return 0;
 }
