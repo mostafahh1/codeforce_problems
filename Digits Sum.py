bc85690936c7876a414dd399945fac88b934d401
mostafahh1 <mmoossttaaffaa6666663@gmail.com>
2026-05-16 19:44:01 -0700

    delete result list & print without saving

--- Digits Sum.py
+++ Digits Sum.py
@@ -1,14 +1,6 @@
 n_testcases = int(input())
-result=[]
-
 
 while n_testcases :
     num = int(input()) + 1
-
-    result.append(int(num/10))
-
+    print(num//10)
     n_testcases -=1
-
-
-for x in result:
-    print(x)
